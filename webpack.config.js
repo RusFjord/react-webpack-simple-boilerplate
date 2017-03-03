7fcfa2490cf7de88678fbcf28f5796e068fbb396
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2017-03-03 15:08:32 +0300

    Added sass-loader and ExtractTextPlugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './frontend/index.js',
     output: {
-        path: path.resolve('public/js/'),
-        filename: 'build.js'
+        path: path.resolve('public/'),
+        filename: 'js/build.js'
     },
     module: {
         loaders: [{
@@ -15,7 +16,15 @@ module.exports = {
             loader: 'babel-loader'
         }, {
             test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader'
+            //loader: 'style-loader!css-loader!sass-loader'
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                //resolve-url-loader may be chained before sass-loader if necessary
+                use: ['css-loader', 'sass-loader']
+            })
         }]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("./css/style.css")
+    ]
 };
