af65c1f1bf4892f97bac9feecae14bde4f373a9a
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2017-03-03 22:14:49 +0300

    Added webpack-dev-server and hot-reload

--- frontend/component/app.js
+++ frontend/component/app.js
@@ -5,6 +5,12 @@ export default class App extends Component {
     render() {
         return(
             <div>
+                <nav className="nav">
+                    <ul>
+                        <li className="nav-item">Первый</li>
+                        <li className="nav-item">Второй</li>
+                    </ul>
+                </nav>
                 <header className="header"/>
                 <div>Привет Мир!!!</div>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 'use strict';
 
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './frontend/index.js',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './frontend/index.js'
+    ],
     output: {
         path: path.resolve('public/'),
-        filename: 'js/build.js'
+        filename: 'js/build.js',
+        publicPath: '/'
     },
     watch: true,
+    devServer: {
+        //hot: true,
+        contentBase: path.resolve('public/'),
+        publicPath: '/'
+    },
     module: {
         loaders: [{
             test: /\.js?$/,
@@ -17,13 +29,16 @@ module.exports = {
             loader: 'babel-loader'
         }, {
             test: /\.scss$/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'sass-loader']
-            })
+            loader: ['style-loader', 'css-loader', 'sass-loader'],
+            // loader: ExtractTextPlugin.extract({
+            //     fallback: 'style-loader',
+            //     use: ['css-loader', 'sass-loader']
+            // })
         }]
     },
     plugins: [
-        new ExtractTextPlugin("./css/style.css")
+        new ExtractTextPlugin("./css/style.css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
     ]
 };
